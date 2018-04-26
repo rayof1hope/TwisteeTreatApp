a5a10b65b4aa32df80796d6f1b8dd71664dffd5e
Gina Warden <ginawarden@jossies-mbp.attlocal.net>
2018-04-25 20:48:04 -0400

    Add OnClick for menu and possible order options

---
+++ app/src/main/java/com/example/android/twisteetreatapp/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.android.twisteetreatapp;
+
+import android.content.res.Configuration;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
